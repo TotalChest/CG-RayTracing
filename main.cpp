9993bc1544f60dd44debcd8afd020a4b32ad3db2
totalchest <13sergey78@mail.ru>
2020-03-04 22:05:15 +0300

    Play with template

--- main.cpp
+++ main.cpp
@@ -7,52 +7,54 @@
 
 #include "Bitmap.h"
 
+#include <cstdlib>
+
 const uint32_t RED   = 0x000000FF;
 const uint32_t GREEN = 0x0000FF00;
 const uint32_t BLUE  = 0x00FF0000;
 
 int main(int argc, const char** argv)
 {
-  std::unordered_map<std::string, std::string> cmdLineParams;
+    std::unordered_map<std::string, std::string> cmdLineParams;
 
-  for(int i=0; i<argc; i++)
-  {
-    std::string key(argv[i]);
-
-    if(key.size() > 0 && key[0]=='-')
+    for(int i=0; i<argc; i++)
     {
-      if(i != argc-1) // not last argument
-      {
-        cmdLineParams[key] = argv[i+1];
-        i++;
-      }
-      else
-        cmdLineParams[key] = "";
+        std::string key(argv[i]);
+
+        if(key.size() > 0 && key[0]=='-')
+        {
+            if(i != argc-1) // not last argument
+            {
+            cmdLineParams[key] = argv[i+1];
+            i++;
+            }
+        else
+            cmdLineParams[key] = "";
+        }
     }
-  }
 
-  std::string outFilePath = "zout.bmp";
-  if(cmdLineParams.find("-out") != cmdLineParams.end())
-    outFilePath = cmdLineParams["-out"];
+    std::string outFilePath = "zout.bmp";
+    if(cmdLineParams.find("-out") != cmdLineParams.end())
+        outFilePath = cmdLineParams["-out"];
 
-  int sceneId = 0;
-  if(cmdLineParams.find("-scene") != cmdLineParams.end())
-    sceneId = atoi(cmdLineParams["-scene"].c_str());
+    int sceneId = 0;
+    if(cmdLineParams.find("-scene") != cmdLineParams.end())
+        sceneId = atoi(cmdLineParams["-scene"].c_str());
 
-  uint32_t color = 0;
-  if(sceneId == 1)
-    color = RED;
-  else if(sceneId == 2)
-    color = RED | GREEN;
-  else if(sceneId == 3)
-    color = BLUE;
+    uint32_t color = 0;
+    if(sceneId == 1)
+        color = RED;
+    else if(sceneId == 2)
+        color = RED | GREEN;
+    else if(sceneId == 3)
+        color = BLUE;
   
-  std::vector<uint32_t> image(512*512); 
-  for(auto& pixel : image)
-    pixel = color;
+    std::vector<uint32_t> image(512*512); 
+    for(auto& pixel : image)
+        pixel = color + (std::rand() % 0x00FFFFFF);
 
-  SaveBMP(outFilePath.c_str(), image.data(), 512, 512);
+    SaveBMP(outFilePath.c_str(), image.data(), 512, 512);
 
-  std::cout << "end." << std::endl;
-  return 0;
+    std::cout << "end." << std::endl;
+    return 0;
 }
